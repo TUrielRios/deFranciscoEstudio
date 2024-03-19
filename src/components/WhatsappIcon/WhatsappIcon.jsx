087949f8241ab24b9003b64971940043d40b5769
unknown <riostiziano6@gmail.com>
2024-03-19 16:45:54 -0300

    mensaje

--- src/components/WhatsappIcon/WhatsappIcon.jsx
+++ src/components/WhatsappIcon/WhatsappIcon.jsx
@@ -6,7 +6,9 @@ const WhatsappIcon = () => {
     // Codifica el mensaje para que sea válido en un URL
 
   return (
-    <a href="https://wa.me/5491167913258?text=Hola!" target="_blank"className={styles.whatsappIcon}>
+    <a href="https://wa.me/5491167913258?text=Hola, estoy interesado en comenzar un proyecto y me gustaría discutir algunos detalles con su equipo
+    ¿Es posible organizar una reunión?"
+     target="_blank"className={styles.whatsappIcon}>
       {/* Aquí puedes poner el icono de WhatsApp */}
       <img src={iconwpp} alt="WhatsApp" />
     </a>
