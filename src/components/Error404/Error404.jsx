162d9130941b00b1baa73165aef43d1155d1aef5
unknown <riostiziano6@gmail.com>
2024-04-06 11:49:59 -0300

    nueva copia

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -5,7 +5,7 @@ import styles from './Error404.module.css'; // Importa los estilos CSS Modules
 const Error404 = () => {
   return (
     <div className={styles.errorContainer}>
-      <h1 className={styles.errorTitle}>Página no encontrada</h1>
+      <h1 className={styles.errorTitle}>Página no encontrada putito</h1>
       <p className={styles.errorMessage}>Lo sentimos, la página que buscas no existe.</p>
       <Link to="/" className={styles.backButton}>Volver al inicio</Link>
     </div>
